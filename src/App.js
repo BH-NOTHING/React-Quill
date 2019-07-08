e70dce55b4ffbf7dd57bd68fc548d40b0f1b47af
Jordan <wangyibo@ih5.cn>
2019-07-08 12:29:17 +0800

    eslint

--- src/App.js
+++ src/App.js
@@ -11,25 +11,31 @@ class App extends Component {
   focus(quill) {
     // console.log(quill);
   }
-  blur(quill){
+  blur(quill) {
     // console.log(quill);
   }
-  textChange(delta, oldDelta, source){
-     console.log(delta)
-     console.log(oldDelta)
-     console.log(source)
+  textChange(delta, oldDelta, source) {
+    console.log(delta);
+    console.log(oldDelta);
+    console.log(source);
   }
-  selectChange(range, oldRange, source){
-     console.log(111)
+  selectChange(range, oldRange, source) {
+    console.log(111);
   }
-    onReady(quill){
-      console.log(quill)
-    }
+  onReady(quill) {
+    console.log(quill);
+  }
   render() {
     return (
       <div className="App">
-        {/*<QuillReact content='[{"attributes":{"color":"#595959","size":"14px","lineheight":"2rem"},"insert":"asdasdasdasd"},{"insert":"\n"},{"attributes":{"color":"#595959","size":"14px","lineheight":"2rem"},"insert":"asdasdasd"},{"insert":"\n"},{"attributes":{"color":"#595959","size":"14px","lineheight":"2rem"},"insert":"asdasdasdasd"},{"insert":"\n"}]'></QuillReact>*/}
-        <QuillReact content='[{"attributes":{"color":"#595959","size":"14px","lineheight":"2rem"},"insert":"asdasdasda啊"},{"attributes":{"size":"14px","color":"#595959","lineheight":"2rem","background":"#ff0000"},"insert":"是大啊是大啊是大"},{"insert":"\n"}]' onFocus={this.focus} onBlur={this.blur} onTextChange={this.textChange} onSelectChange={this.selectChange} onReady={this.onReady}/>
+        <QuillReact
+          content='[{"attributes":{"color":"#595959","size":"14px","lineheight":"2rem"},"insert":"asdasdasda啊"},{"attributes":{"size":"14px","color":"#595959","lineheight":"2rem","background":"#ff0000"},"insert":"是大啊是大啊是大"},{"insert":"\n"}]'
+          onFocus={this.focus}
+          onBlur={this.blur}
+          onTextChange={this.textChange}
+          onSelectChange={this.selectChange}
+          onReady={this.onReady}
+        />
       </div>
     );
   }
